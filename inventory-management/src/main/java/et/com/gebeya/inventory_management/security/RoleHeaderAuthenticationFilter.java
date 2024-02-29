0755aacb4476af4fce567cb2de7840a4deb68d1d
Kindie <kindienega10@gmail.com>
2024-02-29 13:08:20 +0300

    security

---
+++ inventory-management/src/main/java/et/com/gebeya/inventory_management/security/RoleHeaderAuthenticationFilter.java
@@ -0,0 +1,31 @@
+package et.com.gebeya.inventory_management.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class RoleHeaderAuthenticationFilter extends OncePerRequestFilter {
+// adding roleHeaderAuthenticationFilter
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain chain) throws ServletException, IOException {
+        String headerRole = request.getHeader("Role");
+        String roleId = request.getHeader("RoleId");
+        if (headerRole != null && roleId!=null) {
+            Authentication authentication = new RoleHeaderAuthenticationToken(headerRole, roleId);
+            authentication = new RoleHeaderAuthenticationProvider().authenticate(authentication);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+//            System.out.println(headerRole+roleId);
+
+        }
+        chain.doFilter(request, response);
+    }
+}
